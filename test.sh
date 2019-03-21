cc77f72152c73c81783750004cf0de0353a9e86f
Rhanotlq <robertos991@wp.pl>
2019-03-21 15:29:48 +0100

    Changed script language to english.

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ prog=$2
 for f in ${dir}/*.in
 do 
   ./${prog} <$f >temp.out 2>temp.err
-  echo "Dla pliku $f program zakonczyl sie kodem $?."
+  echo "Test $f. The program has ended with exitcode $?."
   if diff ${f%in}out temp.out >/dev/null 2>&1
-	then echo "dla pliku $f zgadza sie std wyjscie"
-	else echo "dla pliku $f wyjscie std sie nie zgadza"
+	then echo "Pass! No differences in out files for ${f%in}out."
+	else echo "FAILURE! Differences found for out files with ${f%in}out."
   fi
   if diff ${f%in}err temp.err >/dev/null 2>&1
-	then echo "dla pliku $f zgadza sie err wyjscie"
-	else echo "dla pliku $f wyjscie err sie nie zgadza"
+	then echo "Pass! No differences in err files for ${f%in}err."
+	else echo "FAILURE! Differences found for err files with ${f%in}err."
   fi
 done
